8e5cc5b7344b91d0120258e6797f2afef278871e
Doug Novellano <douglasnovellano@gmail.com>
2019-12-08 16:08:14 -0500

    improve-modularity-add-tests: Improved unittest for setup_config with defaults mocking file opens and added extra asserts to fully validate current operations of function.

--- tests/test_setup_config.py
+++ tests/test_setup_config.py
@@ -1,9 +1,9 @@
 from unittest import TestCase
 import sys
 if sys.version_info.major == 2:
-    from mock import patch
+    from mock import patch, mock_open, call
 else:
-    from unittest.mock import patch
+    from unittest.mock import patch, mock_open
 from esxi_vm_functions import setup_config
 
 expected_ConfigData = {'HDISK': 20,
@@ -29,6 +29,12 @@ class TestSetup_config(TestCase):
 
     @patch('os.path.expanduser', return_value=".")
     @patch('os.path.exists', return_value=None)
-    def test_setup_config(self, exists_patch, expanduser_patch):
+    @patch('__builtin__.open', new_callable=mock_open, read_data="CPU: 9")
+    def test_setup_config_default(self, open_patch, exists_patch, expanduser_patch):
         ret_ConfigData = setup_config()
         self.assertDictEqual(ret_ConfigData, expected_ConfigData)
+        open_patch.assert_called_with("./.esxi-vm.yml", 'w')
+        exists_patch.assert_called_once()
+        print(dir(expanduser_patch))
+        print(expanduser_patch.mock_calls)
+        expanduser_patch.assert_has_calls([call("~"), call("~")])
