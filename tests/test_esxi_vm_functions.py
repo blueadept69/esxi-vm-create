600b5fdea1795fb82fa078d78f08d2ce282e75ca
Doug Novellano <douglasnovellano@gmail.com>
2019-12-18 19:34:52 -0500

    improve-modularity-add-tests: Improve pylint scores in order to complete testing of mocked objects.

--- tests/test_esxi_vm_functions.py
+++ tests/test_esxi_vm_functions.py
@@ -1,32 +1,38 @@
+"""
+test_esxi_vm_functions.py:
+    unittests for functions/methods in esxi_vm_functions.py module.
+"""
 from unittest import TestCase
 import sys
 import datetime
+
+from esxi_vm_functions import setup_config, SaveConfig, theCurrDateTime, float2human
+
 if sys.version_info.major == 2:
     from mock import patch, mock_open, call
 else:
-    from unittest.mock import patch, mock_open
-from esxi_vm_functions import setup_config, SaveConfig, theCurrDateTime, float2human
+    from unittest.mock import patch, mock_open # pylint: disable=no-name-in-module,import-error,ungrouped-imports
 
-expected_default_ConfigData = {'HDISK': 20,
-                       'LOG': './esxi-vm.log',
-                       'VMXOPTS': '',
-                       'HOST': 'esxi',
-                       'ISO': 'None',
-                       'VIRTDEV': 'pvscsi',
-                       'GUESTOS': 'centos-64',
-                       'NET': 'None',
-                       'PASSWORD': '',
-                       'CPU': 2,
-                       'STORE': 'LeastUsed',
-                       'isDryRun': False,
-                       'isVerbose': False,
-                       'DISKFORMAT': 'thin',
-                       'MEM': 4,
-                       'isSummary': False,
-                       'USER': 'root'
-                      }
+EXPECTED_DEFAULT_CONFIGDATA = {'HDISK': 20,
+                               'LOG': './esxi-vm.log',
+                               'VMXOPTS': '',
+                               'HOST': 'esxi',
+                               'ISO': 'None',
+                               'VIRTDEV': 'pvscsi',
+                               'GUESTOS': 'centos-64',
+                               'NET': 'None',
+                               'PASSWORD': '',
+                               'CPU': 2,
+                               'STORE': 'LeastUsed',
+                               'isDryRun': False,
+                               'isVerbose': False,
+                               'DISKFORMAT': 'thin',
+                               'MEM': 4,
+                               'isSummary': False,
+                               'USER': 'root'
+                              }
 
-test_esxi_vm_yml = """CPU: 9
+TEST_ESXI_VM_YML = """CPU: 9
 DISKFORMAT: thick
 GUESTOS: centos-48
 HDISK: 42
@@ -45,16 +51,16 @@ isSummary: true
 isVerbose: true
 """
 
-good_yml_test_ConfigData = {'HDISK': 42,
-                                'LOG': '/usr/local/logs/esxi-vm.log',
-                                'VMXOPTS': 'vmxopt',
-                                'HOST': 'esxhost',
-                                'ISO': 'centos.iso',
-                                'VIRTDEV': 'notpvscsi',
-                                'GUESTOS': 'centos-48',
-                                'NET': 'test-net',
-                                'PASSWORD': 'passwd',
-                                'CPU': 9,
+GOOD_YML_TEST_CONFIGDATA = {'HDISK': 42,
+                            'LOG': '/usr/local/logs/esxi-vm.log',
+                            'VMXOPTS': 'vmxopt',
+                            'HOST': 'esxhost',
+                            'ISO': 'centos.iso',
+                            'VIRTDEV': 'notpvscsi',
+                            'GUESTOS': 'centos-48',
+                            'NET': 'test-net',
+                            'PASSWORD': 'passwd',
+                            'CPU': 9,
                             'STORE': 'test-ds',
                             'isDryRun': True,
                             'isVerbose': True,
@@ -62,46 +68,57 @@ good_yml_test_ConfigData = {'HDISK': 42,
                             'MEM': 6,
                             'isSummary': True,
                             'USER': 'test-user'
-                            }
+                           }
 
-test_datetime = datetime.datetime(2019, 12, 8, 21, 30, 9, 31532)
+TEST_DATETIME = datetime.datetime(2019, 12, 8, 21, 30, 9, 31532)
 
-class TestSetup_config(TestCase):
+class TestSetupConfig(TestCase):
+    """ Test setup_config function """
 
     @patch('os.path.expanduser', return_value=".")
     @patch('os.path.exists', return_value=None)
     @patch('__builtin__.open', new_callable=mock_open)
     def test_setup_config_default(self, open_patch, exists_patch, expanduser_patch):
+        """ Test mocking user directory to be . and empty config file
+        comparing the result with expected_default_ConfigData """
         ret_configdata = setup_config()
-        self.assertDictEqual(ret_configdata, expected_default_ConfigData)
+        self.assertDictEqual(ret_configdata, EXPECTED_DEFAULT_CONFIGDATA)
         open_patch.assert_called_with("./.esxi-vm.yml", 'w')
         exists_patch.assert_called_once()
         expanduser_patch.assert_has_calls([call("~"), call("~")])
 
     @patch('os.path.expanduser', return_value=".")
     @patch('os.path.exists', return_value=True)
-    @patch('__builtin__.open', new_callable=mock_open, read_data=test_esxi_vm_yml)
+    @patch('__builtin__.open', new_callable=mock_open, read_data=TEST_ESXI_VM_YML)
     def test_setup_config_with_yml(self, open_patch, exists_patch, expanduser_patch):
+        """ Mocking providing test_esxi_vm_yml as test config data, checking that
+        results match good_yml_test_ConfigData """
         ret_configdata = setup_config()
-        self.assertDictEqual(ret_configdata, good_yml_test_ConfigData)
+        self.assertDictEqual(ret_configdata, GOOD_YML_TEST_CONFIGDATA)
         open_patch.assert_called_with("./.esxi-vm.yml", 'w')
         exists_patch.assert_called_once()
         expanduser_patch.assert_has_calls([call("~"), call("~")])
 
     @patch('os.path.expanduser', return_value=".")
     @patch('os.path.exists', return_value=True)
-    @patch('__builtin__.open', new_callable=mock_open, read_data=test_esxi_vm_yml)
+    @patch('__builtin__.open', new_callable=mock_open, read_data=TEST_ESXI_VM_YML)
     @patch('yaml.dump', side_effect=Exception("Test Except"))
-    def test_setup_config_with_exception(self, yaml_patch, open_patch, exists_patch, expanduser_patch):
+    def test_setup_config_with_exception(self, yaml_patch, open_patch,
+                                         exists_patch, expanduser_patch):
+        """ Test, mocking an exception being raised. """
         with self.assertRaises(SystemExit):
             setup_config()
 
 
 class TestSaveConfig(TestCase):
+    """ Test SaveConfig function"""
+
     @patch('os.path.expanduser', return_value=".")
     @patch('__builtin__.open', new_callable=mock_open)
     def test_save_config(self, open_patch, expanduser_patch):
-        ret_val = SaveConfig(good_yml_test_ConfigData)
+        """ Test mocking user directory (.) and file open, validating .esxi-vm.yml file and
+        /usr/local/logs/esi-vm.log file being opened."""
+        ret_val = SaveConfig(GOOD_YML_TEST_CONFIGDATA)
         self.assertEqual(ret_val, 0)
         open_patch.assert_called_with("./.esxi-vm.yml", 'w')
         self.assertIn(call(u'/usr/local/logs/esxi-vm.log'),
@@ -112,28 +129,37 @@ class TestSaveConfig(TestCase):
     @patch('os.path.exists', return_value=True)
     @patch('__builtin__.open', side_effect=Exception("TestExcept"))
     def test_save_config_with_except(self, open_patch, exists_patch, expanduser_patch):
-        ret_val = SaveConfig(good_yml_test_ConfigData)
+        """ Test with file open raising exception. """
+        ret_val = SaveConfig(GOOD_YML_TEST_CONFIGDATA)
         self.assertEqual(ret_val, 1)
         open_patch.assert_called_with("./.esxi-vm.yml", 'w')
 
 
 class TestTheCurrDateTime(TestCase):
+    """ Test theCurrDateTime function """
+
     @patch('datetime.datetime')
-    def test_theCurrDateTime(self, datetime_patch):
-        datetime_patch.now.return_value = test_datetime
+    def test_the_curr_date_time(self, datetime_patch):
+        """ Mock datetime, and validate function returns expected object. """
+        datetime_patch.now.return_value = TEST_DATETIME
         ret_val = theCurrDateTime()
         self.assertEqual('2019-12-08T21:30:09.031532', ret_val)
 
 
 class TestFloat2human(TestCase):
+    """ Test float2human function """
+
     def test_float2human_zero(self):
+        """ Test that 0 returns '0 bytes' """
         ret_val = float2human(0)
         self.assertEqual("0 bytes", ret_val)
 
     def test_float2human_one(self):
+        """ Test that 1 returns '1 byte' """
         ret_val = float2human(1)
         self.assertEqual("1 byte", ret_val)
 
     def test_float2human_more(self):
+        """ Test that 4096 returns '4 kB' """
         ret_val = float2human(4096)
         self.assertEqual("4 kB", ret_val)
